3d704eef9ccfd590e206241226fb681c87cf926a
psclare <pc2620@columbia.edu>
2014-04-14 17:30:27 -0400

    prelim rpam cuda code
    Just the beginning on the CUDA code. NOT TESTED OR COMPLETE.

--- neurokernel/LPU/neurons/photoreceptor.cu
+++ neurokernel/LPU/neurons/photoreceptor.cu
@@ -1,7 +1,10 @@
 #define NNEU &(nneu)d
 #define n_micro 30000.0
-
-__global__ void rpam(int *n_photon)
+// Assume n_photon random from outside the call
+__global__ void rpam(
+    int neu_num,
+    %(type)s *n_photon
+    )
 {
     bool not_converged = true;
     %(type)s lambda_m, n_m;
@@ -22,5 +25,44 @@ __global__ void rpam(int *n_photon)
         }
 
         n_m = n_m_temp;
+    }
+    float lambda_p = n_photon[nid] / n_m;
+    int km = 10 * roundf(lambda_p+1);
+
+    %(type)s p[];
+    %(type)s q[];
+    factorial = 1;
+    float tot_p = 0.0;
+    for(int ii = 0; ii < km; ++ii){
+        if (ii > 0){
+            factorial = factorial * ii;
+        }
+        p[ii] = exp(-lambda_p) * (pow(lambda_p, ii - 1)) / factorial;
+        tot_p += p[ii];
+    }
+
+    float sum_p = 0.0;
+    for(int ii = 0; ii < km; ++ii){
+        for(int jj = 0; jj < ii; ++jj){
+            sum_p += p[ii];
+        }
+        q[ii] = sum_p / (tot_p - p[0]);
+    }
+
+    int r;
+    bool found;
+    int counter;
+    %(type)s n_p[n_micro];
+    for(int ii = 0; ii < n_m; ++ii){
+        int r = // cuRAND somehow
+        found = false;
+        counter = 0;
+        while(!found && counter < q){ //q size, whatever that is
+            if (r < q_counter){
+                n_p[ii] = counter - 2;
+                found = true;
+            }
+            counter += 1;
+        }
     }
 }
