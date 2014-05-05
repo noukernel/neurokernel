e8d85dbd87830200d0c52631a112bd612f2f6b7d
psclare <pc2620@columbia.edu>
2014-05-04 21:03:21 -0400

    deleting dead code

--- neurokernel/LPU/neurons/photoreceptor.py
+++ neurokernel/LPU/neurons/photoreceptor.py
@@ -463,12 +463,7 @@ class Photoreceptor(BaseNeuron):
                 self.X_6.gpudata,\
                 self.X_7.gpudata)
 
-        #temp_I = 0
-        #for ii in xrange(1, len(self.I_in)):
-        #    temp_I += self.I_in[ii]
-
-        #self.I_HH = temp_I/15.7
-	self.I_HH = garray.sum(self.I_in)
+	    self.I_HH = garray.sum(self.I_in)
         print garray.sum(self.I_in)
 
 
